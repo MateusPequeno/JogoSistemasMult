73926a5b7b022f22bd5d090ced7983c103263de2
Mateus Pequeno <wonapack@gmail.com>
2021-08-30 17:23:00 +0000

    Movimento do player exclusivamente horizontal, botão para atirar

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+class Bullet extends Phaser.Physics.Arcade.Sprite
+{
+    constructor (scene, x, y)
+    {
+        super(scene, x, y, 'bullet');
+    }
+
+    fire (x, y)
+    {
+        this.body.reset(x, y);
+
+        this.setActive(true);
+        this.setVisible(true);
+
+        this.setVelocityY(-300);
+    }
+
+    preUpdate (time, delta)
+    {
+        super.preUpdate(time, delta);
+
+        if (this.y <= -32)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
+}
+class Bullets extends Phaser.Physics.Arcade.Group
+{
+    constructor (scene)
+    {
+        super(scene.physics.world, scene);
+
+        this.createMultiple({
+            frameQuantity: 1,
+            key: 'bullet',
+            active: false,
+            visible: false,
+            classType: Bullet
+        });
+    }
+
+    fireBullet (x, y)
+    {
+        let bullet = this.getFirstDead(false);
+
+        if (bullet)
+        {
+            bullet.fire(x, y);
+        }
+    }
+}
+
 class StaffWitch extends Phaser.GameObjects.Sprite {
    constructor (scene, x, y)
     {
@@ -16,62 +70,35 @@ class Obstaculo extends Phaser.GameObjects.Sprite{
         this.setPosition(x, y);
     }  
 }
-/*class TitleScene extends Phaser.Scene{
-      constructor ()
-    {
-        super({ key: 'TitleScene', active: true });
-    }
-
-    create ()
-    {
-      //const text = this.add.text(200,150,'SumikaHell',text.setOrigin(0.5,0.5));
-        this.add.text(50, 60, 'SumikaHell', { font: '16px Courier', fill: '#000000' });
-    }
-}*/
-/*class StoryScene extends Phaser.Scene{
-      constructor ()
-    {
-        super({ key: 'StoryScene', active: true });
-    }
-
-    create ()
-    {
-      //const text = this.add.text(200,150,'SumikaHell',text.setOrigin(0.5,0.5));
-        this.add.text(100, 90, 'a Historia zaz zaz', { font: '16px Courier', fill: '#000000' });
-    }
-}*/
-/*class FirstGameScene extends Phaser.Scene{
-      constructor ()
-    {
-        super({ key: 'StoryScene', active: true });
-    }
-
-    create ()
-    {
-      //const text = this.add.text(200,150,'SumikaHell',text.setOrigin(0.5,0.5));
-        this.add.text(100, 90, 'a Historia zaz zaz', { font: '16px Courier', fill: '#000000' });
-    }
-}*/
-
 function preload() {
     this.load.image('bck', 'assets/BackgroundTeste.png');
-     this.load.image('obstaculo', 'assets/Obstaculo.png');
-   this.load.spritesheet('personagem','assets/sumika.png', { frameWidth:32, frameHeight:48 });
+     this.load.image('bullet', 'assets/bulletTest.png');
+    this.load.image('obstaculo', 'assets/Obstaculo.png');
+    this.load.spritesheet('personagem','assets/sumika.png', { frameWidth:32, frameHeight:48 });
  //  this.load.image('staffwitch', 'assets/staffwitch.png');
-   this.load.spritesheet('staffwitch', 'assets/staffwitch.png',
+    this.load.spritesheet('staffwitch', 'assets/staffwitch.png',
      { frameWidth:150, frameHeight:120 });
+
+  
 }
 
 function create() {
-    this.add.image(191,224,'bck');
-    var personagem = this.physics.add.sprite(200,200,'personagem').setScale(0.65);
+  
 
+    this.add.image(191,224,'bck');
+    var personagem = this.physics.add.sprite(300,300,'personagem').setScale(0.65);
     this.add.existing(new StaffWitch(this, 290,150 ));
     this.add.existing(new StaffWitch(this, 90,150 ));
-    
-    this.add.existing(new Obstaculo(this,100,250)).setScale(1);
-    this.add.existing(new Obstaculo(this,100,310)).setScale(1);
+    this.bullets = new Bullets(this);
+   // this.add.existing(new Obstaculo(this,100,250)).setScale(1);
+    //this.add.existing(new Obstaculo(this,100,310)).setScale(1);
     personagem.setCollideWorldBounds(true);
+
+    /*this.input.on('pointerdown', (pointer) => {
+
+            this.bullets.fireBullet(this.personagem.x, this.personagem.y);
+
+        });*/
     this.anims.create({
       key: 'stopped',
       frames:[{key : 'personagem', frame:4}],
@@ -115,8 +142,12 @@ function create() {
 
 function update() {
     let cursors = this.input.keyboard.createCursorKeys();
+    this.input.keyboard.addCapture('spacebar');
     var personagem = this.personagem;
     var bruxa1 = this.bruxa1;
+    if(cursors.up.isDown){
+      this.bullets.fireBullet(this.personagem.x, this.personagem.y);
+      }
     if(cursors.left.isDown){
       personagem.setVelocityX(-160);
       personagem.anims.play('left',true);
@@ -126,15 +157,6 @@ function update() {
       personagem.anims.play('right',true);
       
       } 
-      else if(cursors.up.isDown){
-      personagem.setVelocityY(-160);
-      personagem.anims.play('up',true);
-
-      }
-      else if(cursors.down.isDown){
-      personagem.setVelocityY(160);
-      personagem.anims.play('down',true);
-      }
         else{
     personagem.setVelocityX(0);
     personagem.setVelocityY(0);
